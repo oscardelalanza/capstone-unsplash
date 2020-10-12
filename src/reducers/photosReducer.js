f126305bcc0899280e6ff4d2d5911174def07464
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-12 14:19:10 -0700

    photos added to the store

--- src/reducers/photosReducer.js
+++ src/reducers/photosReducer.js
@@ -1,5 +1,9 @@
+import { ADD_PHOTOS } from '../js/literals';
+
 const photoReducer = (state = [], action) => {
   switch (action.type) {
+    case ADD_PHOTOS:
+      return [...action.photos];
     default:
       return state;
   }
