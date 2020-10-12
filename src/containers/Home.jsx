73ed1d1fe5d43bd3c9070ebe76099a7dd325aef2
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-12 15:11:08 -0700

    spinner added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {
   Button, Container, Form, FormControl,
 } from 'react-bootstrap';
+import Animation from '../components/Animation';
 
 const Home = () => (
   <Container fluid className="page mt-lg-4">
-    <Container fluid className="d-lg-flex justify-content-lg-between">
+    <Container fluid className="d-lg-flex justify-content-lg-between p-lg-0">
       <h5 className="text-light font-weight-light">Explore Amazing Photos</h5>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-lg-2" />
         <Button variant="outline-primary">Search</Button>
       </Form>
     </Container>
+    <Animation />
   </Container>
 );
 
