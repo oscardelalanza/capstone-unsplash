26380478568bb9d6c465a4cdcd545f470dfd2337
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-12 08:25:53 -0700

    margin on about fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const About = () => (
   <Container className="page mt-lg-4">
     <h1 className="text-center gradient-color-6">About Me</h1>
-    <Container className="text-justify text-light">
+    <Container className="text-justify text-light mt-md-3">
       <p>
         This must contain a description of the developer
       </p>
