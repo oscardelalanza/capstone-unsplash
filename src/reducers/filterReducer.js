784dde21eb81176f8da3765af567ce27ca11804b
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-13 15:50:01 -0700

    filter reducer updated

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,5 +1,9 @@
+import { CHANGE_FILTER } from '../js/literals';
+
 const filterReducer = (state = '', action) => {
   switch (action.type) {
+    case CHANGE_FILTER:
+      return action.filter;
     default:
       return state;
   }
