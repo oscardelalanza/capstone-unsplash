10846b1d8f9a1cab051acc79040574aaa4d8d67c
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-12 17:58:27 -0700

    validation for status

--- src/actions/photosActions.js
+++ src/actions/photosActions.js
@@ -1,14 +1,24 @@
 import { toJson } from 'unsplash-js';
 import unsplash from '../js/unsplash';
-import { ADD_PHOTOS } from '../js/literals';
+import { ADD_PHOTOS, LOADING, OK } from '../js/literals';
 
 export const addPhotos = photos => ({
   type: ADD_PHOTOS,
   photos,
 });
 
+export const loading = () => ({
+  type: LOADING,
+});
+
+export const ok = () => ({
+  type: OK,
+});
+
 export const getNewPhotos = () => (dispatch => {
+  dispatch(loading());
   unsplash.photos.listPhotos(1, 20, 'latest').then(toJson).then(json => {
     dispatch(addPhotos(json));
+    dispatch(ok());
   });
 });
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,19 +2,37 @@ import React from 'react';
 import {
   Button, Container, Form, FormControl,
 } from 'react-bootstrap';
+import { connect } from 'react-redux';
 import Animation from '../components/Animation';
+import { LOADING } from '../js/literals';
 
-const Home = () => (
-  <Container fluid className="page mt-lg-4">
-    <Container fluid className="d-lg-flex justify-content-lg-between p-lg-0">
-      <h5 className="text-light font-weight-light">Explore Amazing Photos</h5>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-lg-2" />
-        <Button variant="outline-primary">Search</Button>
-      </Form>
+const Home = (props) => {
+  const { photos, status } = props;
+  let component;
+
+  if (status === LOADING) {
+    component = <Animation />;
+  } else {
+    component = 'images';
+  }
+
+  return (
+    <Container fluid className="page mt-lg-4">
+      <Container fluid className="d-lg-flex justify-content-lg-between p-lg-0">
+        <h5 className="text-light font-weight-light">Explore Amazing Photos</h5>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-lg-2" />
+          <Button variant="outline-primary">Search</Button>
+        </Form>
+      </Container>
+      { component }
     </Container>
-    <Animation />
-  </Container>
-);
+  );
+};
 
-export default Home;
+const mapStateToProps = state => ({
+  photos: state.photos,
+  status: state.status,
+});
+
+export default connect(mapStateToProps)(Home);
