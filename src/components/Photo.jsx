7a8ba6d11ad1dbce641a6b0e0a9ab9dc5aec7833
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-13 10:01:20 -0700

    photo description added

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 const Photo = props => {
@@ -10,13 +10,20 @@ const Photo = props => {
   return (
     <Container className="mt-lg-4">
       <h5 className="text-white font-weight-light mb-lg-3">
-        Photo by:
-        {` ${photo.user.name}`}
+        Photo by:{` ${photo.user.name}`}
       </h5>
       <Card>
         <Card.Img variant="top" src={photo.urls.regular} />
         <Card.Body>
           <Card.Title>Photo Description</Card.Title>
+          <Card.Text>{photo.description}</Card.Text>
+          <Card.Text>{photo.alt_description}</Card.Text>
+          <Card.Text>Size: {`${photo.width}x${photo.height}`}</Card.Text>
+          <Card.Text>
+            Download this photo:
+            {' '}
+            <Button href={`${photo.links.download}?force=true`} download variant="success" size="sm">Download</Button>
+          </Card.Text>
         </Card.Body>
       </Card>
     </Container>
